557a1cc4a4387a579a3a13eb721516bdd227fae4
Dalsen-Chung <codedalsen@163.com>
2020-03-24 14:56:44 +0800

    提交更改

--- generator/template/src/main.js
+++ generator/template/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 import 'normalize.css'
 import 'amfe-flexible/index.js'
-import './plugins/vant.js'
-import './icon' // svg-icon
+import '@/plugins/vant.js'
+import '@/icon' // svg-icon
 
 Vue.config.productionTip = false
 
